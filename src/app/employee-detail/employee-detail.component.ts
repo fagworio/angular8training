3af8e277ff1efd35c34e23bf71d0750fb458169f
unknown <fagworio@gmail.com>
2019-08-01 22:03:59 -0300

    handler erros

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -5,6 +5,7 @@ import { EmployeeService } from '../employee.service';
   selector: 'app-employee-detail',
   template: `
     <h1>Employees Detail</h1>
+    <h3>{{errorMsg}}</h3>
     <ul *ngFor="let employee of employees">
       <li>{{employee.id}} {{employee.name}} {{employee.age}} </li>
     </ul>
@@ -13,11 +14,13 @@ import { EmployeeService } from '../employee.service';
 })
 export class EmployeeDetailComponent implements OnInit {
   public employees  = []
+  public errorMsg;
   constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
     this._employeeService.getEmployees()
-          .subscribe(data => this.employees = data);
+          .subscribe(data => this.employees = data,
+            error => this.errorMsg = error);
   }
 
 }
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IEmployee } from './employee';
-import { Observable } from 'rxjs';
+import { throwError, Observable } from 'rxjs'
+import { catchError } from 'rxjs/operators'
+ 
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
-  private _url: string = "/assets/data/employee.json";
+  private _url: string = "/assets/data/employee.json1";
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<IEmployee[]> {
     return this.http.get<IEmployee[]>(this._url)
+                .pipe(catchError(this.errorHandler))
+  } 
+  errorHandler(error: HttpErrorResponse) {
+    return throwError(error.message || "Server Error");
   }
 
 }
